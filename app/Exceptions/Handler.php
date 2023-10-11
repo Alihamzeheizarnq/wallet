755ace58980eb2e7b18b1697ac423fb28f1feae4
alihamzehei <alihamzehei2017@gmail.com>
2023-10-11 18:39:19 +0330

    feat:updated the erros

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,12 +2,17 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponse;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Throwable;
 use Exception;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponse;
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -26,5 +31,23 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
         });
+    }
+
+    public function render($request, $exception)
+    {
+        if ($exception instanceof BadRequestException){
+            $error = $exception->getMessage();
+
+            return $this->errorResponse(message: $error , status: Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($exception instanceof ValidationException){
+
+            $errors = $exception->validator->errors();
+
+            return $this->errorResponse(errors: $errors->toArray() , status: Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+       return parent::render($request, $exception);
     }
 }
