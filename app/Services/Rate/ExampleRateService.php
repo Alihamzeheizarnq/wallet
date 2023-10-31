d9529fc9843f69530a184c802d06353d3d2dbad6
alihamzehei <alihamzehei2017@gmail.com>
2023-10-31 19:08:51 +0330

    feat:updated

--- app/Services/Rate/ExampleRateService.php
+++ app/Services/Rate/ExampleRateService.php
@@ -15,11 +15,28 @@ class ExampleRateService implements RateInterface
 
     public function getData(?string $currencyKey): Collection
     {
-        // TODO: Implement getData() method.
+       return collect([
+          [
+              'key' => 'usd',
+              'value' => 56000,
+              'date' => '2023-01-01 00:00:00'
+          ],
+           [
+               'key' => 'irr',
+               'value' => 40000,
+               'date' => '2023-01-01 00:00:00'
+           ],
+           [
+               'key' => 'jpr',
+               'value' => 10000,
+               'date' => '2023-01-02 00:00:00'
+           ],
+
+       ]);
     }
 
     public function formatted(): Collection
     {
-        // TODO: Implement formatted() method.
+        return $this->getData();
     }
 }
